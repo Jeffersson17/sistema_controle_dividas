c4fb047cf857a2ba942f7d378a2bb8e5b58a2cde
Jeffersson <barrosjefferson407@gmail.com>
2025-08-10 21:07:19 -0300

    add new route

--- django/debt_control/urls.py
+++ django/debt_control/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
 
-from client.views import ClientViewSet, HistoricalViewSet
+from client.views import ClientViewSet, HistoricalViewSet, RegisterUser
 
 from rest_framework import routers
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
@@ -15,4 +15,5 @@ urlpatterns = [
     path("token/", TokenObtainPairView.as_view()),
     path("token/refresh/", TokenRefreshView.as_view()),
     path("admin/", admin.site.urls),
+    path("api/register/", RegisterUser.as_view(), name='register_user'),
 ]
